7b9907a86914a48466ba5f200d4eb41c8ebaa3a9
Bogdan Buescu <bbuescu@pentalog.com>
2022-08-29 18:36:12 +0300

    - added saga middleware

--- src/containers/Users/middleware.js
+++ src/containers/Users/middleware.js
@@ -1,22 +1,43 @@
 import { fetchUsersApi } from "../../api";
 import { fetchUsersFailed, fetchUsersSuccess } from "./actions";
 import { FETCH_USERS_REQUEST } from "./reducer";
+import { all, call, put, takeLatest } from "redux-saga/effects";
 
-const fetchUsersMiddleware =
-  ({ dispatch }) =>
-  (next) =>
-  (action) => {
-    next(action);
+export function* fetchUsersMiddleware() {
+  yield takeLatest(FETCH_USERS_REQUEST, fetchUsersMiddlewareHandler);
+}
 
-    if (action.type === FETCH_USERS_REQUEST) {
-      fetchUsersApi()
-        .then((resp) => {
-          dispatch(fetchUsersSuccess(resp));
-        })
-        .catch((e) => {
-          dispatch(fetchUsersFailed(e.message));
-        });
-    }
-  };
+export function* fetchUsersMiddlewareHandler(action) {
+  try {
+    debugger;
+    const resp = yield call(fetchUsersApi, 2);
+    yield put(fetchUsersSuccess(resp));
+  } catch (e) {
+    yield put(fetchUsersFailed(e.message));
+  }
+}
 
-  export default [fetchUsersMiddleware];
+export function* usersSaga() {
+  yield all([fetchUsersMiddleware()]);
+}
+
+export default usersSaga;
+
+// const fetchUsersMiddleware =
+//   ({ dispatch }) =>
+//   (next) =>
+//   (action) => {
+//     next(action);
+
+//     if (action.type === FETCH_USERS_REQUEST) {
+// fetchUsersApi()
+//   .then((resp) => {
+//     dispatch(fetchUsersSuccess(resp));
+//   })
+//   .catch((e) => {
+//     dispatch(fetchUsersFailed(e.message));
+//   });
+//   }
+// };
+
+// export default [fetchUsersMiddleware];
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,13 @@
+import { all } from 'redux-saga/effects';
 import usersMiddleare from './containers/Users/middleware';
-import clientsMiddleare from './containers/Clients/middleware';
+// import clientsMiddleare from './containers/Clients/middleware';
 
 
-export default [...usersMiddleare, ...clientsMiddleare];
+// export default [...usersMiddleare, ...clientsMiddleare];
+
+export function* rootSaga() {
+    yield all([usersMiddleare()]);
+}
+
+
+export default rootSaga;
